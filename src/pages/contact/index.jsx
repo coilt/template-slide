d200abe66f84ba27f06bfef7397cba4255c27b68
coilt <cameraboi@gmail.com>
2024-02-09 16:28:24 +0700

    Fix quotes and apostrophes

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -9,11 +9,11 @@ export default function Contact() {
       <h1>Contact</h1>
       <div className='body'>
         <p>
-          Let me go, you don&apos;t understand what will happen. My brothers-" The
+          Let me go, you don&apos;t understand what will happen. My brothers. The
           Lord Cleric punched her. Her head flew back and a spray of blood wet
           the dry mud and spattered over the leaves concealing me. Face wet with
           tears and whimpering, she tried to crawl toward the trees and dragged
-          up clumps of earth with her fingernails. "You must let me go." The
+          up clumps of earth with her fingernails. &ldquo;You must let me go.&rdquo; The
           words sounded muffled, like she had a mouthful of something foul. The
           Lord Cleric executed a neat half turn and stamped on her thigh. There
           was a sharp snap, like I&apos;d picked up a twig and yanked on the ends
